3162d4c25e447ec7b94c24e2702081b62920a66b
Yannick Carissan <yannick.carissan@univ-amu.fr>
2019-05-03 16:04:57 +0200

    use of numpy

--- dot2xyz.py
+++ dot2xyz.py
@@ -3,6 +3,7 @@ import logging
 import argparse
 import pydot
 import math
+import numpy
 
 logging.basicConfig(filename='dot2xyz.log', level=logging.DEBUG)
 
@@ -18,30 +19,25 @@ def processArguments():
 
 def center(geom):
     """Put the barycenter of the molecule at origin"""
-    xmoy = ymoy = 0
+    center = numpy.array([0,0,0])
     for at in geom.keys():
-        xmoy = xmoy + geom[at]["x"]
-        ymoy = ymoy + geom[at]["y"]
-    xmoy = xmoy / len(geom.keys())
-    ymoy = ymoy / len(geom.keys())
+        center = center + geom[at]["pos"]
+    center = center / len(geom.keys())
     for at in geom.keys():
-        geom[at]["x"] = geom[at]["x"] - xmoy
-        geom[at]["y"] = geom[at]["y"] - ymoy
+        geom[at]["pos"] = geom[at]["pos"] - center
 
 
 def scale(geom):
     """scale the geometry such that the first C-C bond equals a reasonable value"""
     reasonable_value = 1.4
     at0 = list(geom.keys())[0]
-    (x0, y0, z0) = (geom[at0]["x"], geom[at0]["y"], geom[at0]["z"])
+    p0 = geom[at0]["pos"]
     at1 = list(geom.keys())[1]
-    (x1, y1, z1) = (geom[at1]["x"], geom[at1]["y"], geom[at1]["z"])
-    dist = math.sqrt(math.pow((x0 - x1), 2) + math.pow((y0 - y1), 2) + math.pow((z0 - z1), 2))
+    p1 = geom[at1]["pos"]
+    dist = numpy.linalg.norm(p0-p1)
     scale = reasonable_value / dist
     for at in geom.keys():
-        geom[at]["x"] = geom[at]["x"] * scale
-        geom[at]["y"] = geom[at]["y"] * scale
-        geom[at]["z"] = geom[at]["z"] * scale
+        geom[at]["pos"] = geom[at]["pos"] * scale
     return geom
 
 
@@ -52,14 +48,15 @@ def main():
     for node in dot_graph.get_nodes():
         "attribute 'pos' comes with double quotes"
         (x, y) = [float(a) for a in node.get('pos').replace('"', "").split(',')]
-        geom[node.get_name()] = {'x': x, 'y': y, 'z': 0}
-        print(node.get_name(), x, y)
+        pos=numpy.array([x,y,0])
+        geom[node.get_name()] = {'pos': pos}
+        print(node.get_name(), pos)
     center(geom)
     scale(geom)
 
     print(len(geom), '\n')
     for at in geom.keys():
-        print("C", geom[at]["x"], geom[at]["y"], geom[at]["z"])
+        print("C", geom[at]["pos"])
 
 
 if __name__ == '__main__':
